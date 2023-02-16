2992b09377662e241e0c0352b5d2e14fe3b28af5
Appiah Collins <appiahc86@gmail.com>
2023-02-16 12:56:12 +0000

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -245,34 +245,6 @@ const stakeNow = async () => {
       </ul>
     </div >
 
-<!--<template v-if="ads.length">-->
-<!--  <div id="carouselExampleIndicators" class="carousel slide">-->
-<!--    <div class="carousel-indicators">-->
-<!--      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>-->
-<!--      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>-->
-<!--      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>-->
-<!--    </div>-->
-<!--    <div class="carousel-inner">-->
-<!--      <div class="carousel-item active">-->
-<!--        <img src="/img/days/lucky-tuesday.png" class="d-block w-100" alt="...">-->
-<!--      </div>-->
-<!--      <div class="carousel-item">-->
-<!--        <img src="/img/days/fortune-thursday.png" class="d-block w-100" alt="...">-->
-<!--      </div>-->
-<!--      <div class="carousel-item">-->
-<!--        <img src="/img/days/monday-special.png" class="d-block w-100" alt="...">-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">-->
-<!--      <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-<!--      <span class="visually-hidden">Previous</span>-->
-<!--    </button>-->
-<!--    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">-->
-<!--      <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-<!--      <span class="visually-hidden">Next</span>-->
-<!--    </button>-->
-<!--  </div>-->
-<!--</template>-->
 
   <!-- ........... Carousel ............... -->
     <Carousel :value="ads" :numVisible="1" :numScroll="1" :circular="true" :showNavigators="false"
@@ -301,7 +273,8 @@ const stakeNow = async () => {
             <h6 class="text-center" v-else>
               Status: <span class="text-success">Open</span>
             </h6>
-            <span>{{ today ? moment(today).format('Do MMM YYYY, h:mm:ss a') : '' }}</span>
+            <span v-if="today">{{ today ? moment(today).format('Do MMM YYYY, h:mm:ss a') : '' }}</span>
+            <span v-else class="invisible">.............</span>
           </div>
           <div class="card-body">
             <div class="game-images-wrapper">
